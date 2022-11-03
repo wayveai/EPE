38e19116565bb267009a4d0b18256f1d9066f363
kacper <kacper@wayve.ai>
2022-11-03 22:04:55 +0000

    dev utils

--- epe/dev_utils/file_filter.py
+++ epe/dev_utils/file_filter.py
@@ -9,16 +9,28 @@ def remove_lines(file_path, excluded_keywords):
 
     os.replace(temp_output, file_path)
 
-dark_worlds = [
-'2022-10-05--18-52-55--somerstown-aft-loop-anti-clockwise-v1--c92624ba44dfa9c9--44c44ade',
-'2022-10-05--21-09-30--somerstown-aft-loop-anti-clockwise-v1--5b4e723801f6a739--58281ec4',
-'2022-10-06--05-10-54--somerstown-aft-loop-clockwise-v1--f56e08f1fa0404db--25c23d78',
-'2022-10-06--04-01-47--somerstown-aft-loop-clockwise-v1--ea2e41b460f96ae7--9dedfe93',
-]
+def keep_lines(file_path, out_file_path, keywords):
+    with open(file_path, 'r') as file_in:
+        with open(out_file_path, "w") as file_out:
+            file_out.writelines(filter(lambda line: any([x in line for x in keywords]), file_in))
 
-files = ['/home/kacper/data/EPE/somers_town/sim_files.csv']
-for file in files:
-    remove_lines(file, dark_worlds)
+# %%
 
+# dark_worlds = [
+# '2022-10-05--18-52-55--somerstown-aft-loop-anti-clockwise-v1--c92624ba44dfa9c9--44c44ade',
+# '2022-10-05--21-09-30--somerstown-aft-loop-anti-clockwise-v1--5b4e723801f6a739--58281ec4',
+# '2022-10-06--05-10-54--somerstown-aft-loop-clockwise-v1--f56e08f1fa0404db--25c23d78',
+# '2022-10-06--04-01-47--somerstown-aft-loop-clockwise-v1--ea2e41b460f96ae7--9dedfe93',
+# ]
+
+# files = ['/home/kacper/data/EPE/somers_town/sim_files.csv']
+# for file in files:
+#     remove_lines(file, dark_worlds)
+
+
+# %%
+file = '/home/kacper/data/EPE/somers_town/sim_files.csv'
+runs = ['2022-10-05--13-11-03--somerstown-aft-loop-anti-clockwise-v1--f0e9b72c9fb9bb9f--5aae8a99']
+keep_lines(file, '/home/kacper/data/EPE/somers_town/video_test.csv', runs)
 
 # %%
