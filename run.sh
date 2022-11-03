f579e414f13b0254d4d2459e4ffba1f8744c666c
kacper <kacper@wayve.ai>
2022-11-03 22:02:59 +0000

    data pre-processing script currently for somers-town-weather-ds

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
-python epe/matching/feature_based/collect_crops.py sim /home/kacper/data/EPE/somers_town/sim_files.csv --out_dir /home/kacper/data/EPE/somers_town --data_root /home/kacper/data/datasets
-python epe/matching/feature_based/collect_crops.py real /home/kacper/data/EPE/somers_town/real_files.csv --out_dir /home/kacper/data/EPE/somers_town --data_root /home/kacper/data/datasets
+touch /home/kacper/progress
+python epe/matching/feature_based/collect_crops.py sim /home/kacper/code/EPE/datasets/somers_town/sim_files.csv --out_dir /home/kacper/code/EPE/datasets/somers_town --data_root /home/kacper/data/datasets
+echo "sim crops collected" >> /home/kacper/progress
+python epe/matching/feature_based/collect_crops.py real /home/kacper/code/EPE/datasets/somers_town/real_files.csv --out_dir /home/kacper/code/EPE/datasets/somers_town --data_root /home/kacper/data/datasets
+echo "real crops collected" >> /home/kacper/progress
 
-python epe/matching/feature_based/find_knn.py /home/kacper/data/EPE/somers_town/crop_sim.npz /home/kacper/data/EPE/somers_town/crop_real.npz /home/kacper/data/EPE/somers_town/knn_sim-real.npz -k 10
-python epe/matching/filter.py /home/kacper/data/EPE/somers_town/knn_sim-real.npz /home/kacper/data/EPE/somers_town/crop_sim.csv /home/kacper/data/EPE/somers_town/crop_real.csv 1.0 /home/kacper/data/EPE/somers_town/matched_crops_sim-real.csv
+python epe/matching/feature_based/find_knn.py /home/kacper/code/EPE/datasets/somers_town/crop_sim.npz /home/kacper/code/EPE/datasets/somers_town/crop_real.npz /home/kacper/code/EPE/datasets/somers_town/knn_sim-real.npz -k 10
+echo "knn done" >> /home/kacper/progress
+python epe/matching/filter.py /home/kacper/code/EPE/datasets/somers_town/knn_sim-real.npz /home/kacper/code/EPE/datasets/somers_town/crop_sim.csv /home/kacper/code/EPE/datasets/somers_town/crop_real.csv 1.0 /home/kacper/code/EPE/datasets/somers_town/matched_crops_sim-real.csv
+echo "filter done" >> /home/kacper/progress
 
-python epe/matching/compute_weights.py /home/kacper/data/EPE/somers_town/matched_crops_sim-real.csv 600 960 /home/kacper/data/EPE/somers_town/crop_weights_sim-real.npz
+
+python epe/matching/compute_weights.py /home/kacper/code/EPE/datasets/somers_town/matched_crops_sim-real.csv 600 960 /home/kacper/code/EPE/datasets/somers_town/crop_weights_sim-real.npz
+echo "weights coimputed" >> /home/kacper/progress
