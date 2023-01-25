14410164b220b46168534351e895b8b1e7caf25c
kacper <kacper@wayve.ai>
2023-01-25 17:20:17 +0000

    chaning paths to csv files

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 touch /home/kacper/progress
-python epe/matching/feature_based/collect_crops.py sim /home/kacper/data/datasets/urban-driving/sim_files.csv --out_dir /home/kacper/code/EPE/datasets/urban-driving --data_root /home/kacper/data/datasets/urban-driving/
+python epe/matching/feature_based/collect_crops.py sim /home/kacper/code/EPE/datasets/urban-driving/sim_files.csv --out_dir /home/kacper/code/EPE/datasets/urban-driving
 echo "sim crops collected" >> /home/kacper/progress
-python epe/matching/feature_based/collect_crops.py real /home/kacper/data/datasets/urban-driving/real_files.csv --out_dir  /home/kacper/code/EPE/datasets/urban-driving  --data_root /home/kacper/data/datasets/urban-driving/
+python epe/matching/feature_based/collect_crops.py real /home/kacper/code/EPE/datasets/urban-driving/real_files.csv --out_dir  /home/kacper/code/EPE/datasets/urban-driving
 echo "real crops collected" >> /home/kacper/progress
 
-python epe/matching/feature_based/find_knn.py /home/kacper/code/EPE/datasets/urban-driving/crop_sim.npz /home/kacper/code/EPE/datasets/urban-driving/crop_real.npz /home/kacper/code/EPE/datasets/urban-driving/knn_sim-real.npz -k 10
+python epe/matching/feature_based/find_knn.py /home/kacper/code/EPE/datasets/urban-driving/crop_sim.npz /home/kacper/code/EPE/datasets/urban-driving/crop_real.npz /home/kacper/code/EPE/datasets/urban-driving/knn_sim-real.npz -k 5
 echo "knn done" >> /home/kacper/progress
 python epe/matching/filter.py /home/kacper/code/EPE/datasets/urban-driving/knn_sim-real.npz /home/kacper/code/EPE/datasets/urban-driving/crop_sim.csv /home/kacper/code/EPE/datasets/urban-driving/crop_real.csv 1.0 /home/kacper/code/EPE/datasets/urban-driving/matched_crops_sim-real.csv
 echo "filter done" >> /home/kacper/progress
